6ed3c07680006eccc5c6d108c15307d0bb230c88
Stuart Raynor <sturaynor@gmail.com>
2022-10-17 14:34:34 +0100

    Add types

--- app/services/TeamService.scala
+++ app/services/TeamService.scala
@@ -2,9 +2,11 @@ package services
 
 import models.Team
 
+import scala.util.Try
+
 trait TeamService {
-  def create(team: Team)
-  def update(team: Team)
+  def create(team: Team): Unit
+  def update(team: Team): Try[Team]
   def findById(id: Long): Option[Team]
   def findAll(): List[Team]
   def findByName(name: String): Option[Team]
