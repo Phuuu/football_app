44d98b38fdb9b319ca34fb817b58a8f5151e9fd0
Stuart Raynor <sturaynor@gmail.com>
2022-10-18 11:43:49 +0100

    Created Async stadium service file with implmentation

---
+++ app/services/AsyncStadiumService.scala
@@ -0,0 +1,22 @@
+package services
+
+import models.Stadium
+import org.mongodb.scala.Document
+
+import scala.concurrent.Future
+
+trait AsyncStadiumService {
+
+  def create(stadium: Stadium): Unit
+
+  def update(id: Long, doc: Document): Future[Stadium]
+
+  def findById(id: Long): Future[Option[Stadium]]
+
+  def findAll(): Future[List[Stadium]]
+
+  def findByCountry(country: String): Future[List[Stadium]]
+
+  def findByName(name: String): Future[Option[Stadium]]
+
+}
