14402e114b3409e7c4c5535db9a147ffaf31d38a
Stuart Raynor <sturaynor@gmail.com>
2022-10-18 15:30:13 +0100

    Update microservice image and updated stadiumservice

--- app/services/MongoDBStadiumService.scala
+++ app/services/MongoDBStadiumService.scala
@@ -1,5 +1,34 @@
 package services
+import models.Stadium
+import org.mongodb.scala.{Document, MongoClient}
+
+import scala.concurrent.Future
 
 class MongoDBStadiumService extends AsyncStadiumService {
+  val mongoClient: MongoClient = MongoClient("mongodb://mongo-root:mongo-password@localhost:" + 27017)
+  val myCompanyDatabase = mongoClient.getDatabase("football_app")
+  val stadiumCollection = myCompanyDatabase.getCollection("stadium")
+  override def create(stadium: Stadium): Unit = {
+    val newStadium = stadiumDocument(stadium)
+    stadiumCollection.insertOne(newStadium).map(x => x.getInsertedId)
+  }
 
+  override def update(id: Long, doc: Document): Future[Stadium] = ???
+
+  override def findById(id: Long): Future[Option[Stadium]] = ???
+
+  override def findAll(): Future[List[Stadium]] = ???
+
+  override def findByCountry(country: String): Future[List[Stadium]] = ???
+
+  override def findByName(name: String): Future[Option[Stadium]] = ???
+
+  def stadiumDocument(x: Stadium): Document = {
+    Document(
+      "_id" -> x.id,
+      "name" -> x.name,
+      "country" -> x.country,
+      "seats" -> x.seats
+    )
+  }
 }
